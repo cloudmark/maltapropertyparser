19e1d75fa8283dec8f36362fe02b671cf6cb84e8
Mark Galea <mmgalea@gmail.com>
2013-01-14 12:51:22 +0000

    Updated Frank Salt Parser

--- grabFrankSaltPage.sh
+++ grabFrankSaltPage.sh
@@ -1,48 +1,121 @@
 #!/bin/bash
 TEMP_DIR=./temp/franksalt
+COOKIE_DIR=./temp/franksalt/cookies
 rm -rf $TEMP_DIR
 mkdir -p $TEMP_DIR
-page=1
-	echo "Parsing Page -> $page"
-	TEMP_FILE="$TEMP_DIR/temp_$page.html"
+mkdir -p $COOKIE_DIR
+
+# Determine whether there are any more results.  
+# @return NEXT variable.  
+function moreResults {
+	NEXT=`php -f regexFinder.php ./temp/franksalt/temp_1.html '/<span id="content_lblResultSummary">[^<]*<\/span>/'`
+	NEXT=`echo "$NEXT" | tr -dC [0-9\ ] | awk '{if ($2 == $3) {print 0} else {print 1} }'`
+	echo "Has More: $NEXT"
+}
+
+
+# Set temporary file. 
+# @return TEMP_FILE variable.  
+# @return PREVIOUS_PAGE variable
+# @return PREVIOUS_TEMP_FILE variable.  
+function setTempFile {
+	PAGE=$1
+	
+	# Set the previous page variable too.   
+	PREVIOUS_PAGE=$(($PAGE-1))
+	if [ "$PREVIOUS_PAGE" -lt "0" ] 
+	then 
+		PREVIOUS_PAGE=$PAGE
+	fi
+	
+	PREVIOUS_TEMP_FILE="$TEMP_DIR/temp_""$PREVIOUS_PAGE"".html"
+	TEMP_FILE="$TEMP_DIR/temp_$PAGE.html"
 	echo "" > $TEMP_FILE
 	echo "Created Temp File: $TEMP_FILE"
-	# echo "CurrentPage:$page" >> $TEMP_FILE
-	cp queryFrankSaltParam.txt $TEMP_FILE
-	QUERY_STRING=`cat $TEMP_FILE | sed '/^$/d' | grep -v "#" | tr '\r\n' '&' | tr ':' '='`
-	QUERY_STRING="$QUERY_STRING"'ctl00$content$gvpDataPager$ddlPageIndex='"$page"
-	echo $QUERY_STRING
-	curl -s --cookie-jar cookies.txt --data "$QUERY_STRING" "http://www.franksalt.com.mt/Search/SearchResults.aspx" > "$TEMP_FILE"_bak
+	# TEMP_FILE being returned. 
+}
+
+# Grabs a page from a URL, this function manages its own cookies
+#  
+function grabPage { 
+	PAGE=$1
+	echo "Parsing Page -> $PAGE" 
+	setTempFile "$PAGE"
+	
+	# We always save the page cookies.  
+	SAVE_COOKIES="cookies_""$PAGE"".txt"
+	if [ "$PAGE" -eq "1" ]
+	then
+		echo "First Page Detected... Reading Query Variables.  "
+		# It is the first page thus we load the parameters which the user gave.    
+		cp queryFrankSaltParam.txt $TEMP_FILE
+		QUERY_STRING=`cat $TEMP_FILE | sed '/^$/d' | grep -v "#" | tr '\r\n' '&' | tr ':' '='`
+		QUERY_STRING="$QUERY_STRING"'ctl00$content$gvpDataPager$ddlPageIndex='"$page"
+		LOAD_COOKIES=""
+	else
+		# It is the next page, we need the url encoded form from the previous page and the cookies.  
+		LOAD_COOKIES="cookies_""$PREVIOUS_PAGE"".txt"
+		echo "Form Resubmission Detected ... [Previous Page: $PREVIOUS_PAGE] [Previous Temp File: $PREVIOUS_TEMP_FILE] [Cookies: $LOAD_COOKIES]"
+		# This function will override the query string.  
+		grabFormFields "$PREVIOUS_TEMP_FILE"
+	fi
+
+	# Grab the URL with the updates.  
+	grabUrl "$PAGE" "$QUERY_STRING" 'http://www.franksalt.com.mt/Search/SearchResults.aspx' "$TEMP_FILE" "$LOAD_COOKIES" "$SAVE_COOKIES" 
+	
+}
+
+function grabUrl {
+	PAGE=$1
+	QUERY_STRING=$2
+	URL=$3
+	TEMP_FILE=$4
+	LOAD_COOKIE=$5
+	SAVE_COOKIE=$6
+	
+	if [[ "$LOAD_COOKIES" == "" ]] 
+	then
+		LOAD_COOKIE=`mktemp -t cookie`
+		# Start off the file. 
+		echo "" > $LOAD_COOKIE
+		echo "WARNING: Load cookie file not found, Created Temp File [$LOAD_COOKIE]"
+	fi
+	
+	if [[ "$SAVE_COOKIES" == "" ]]
+	then
+		SAVE_COOKIES=`mktemp -t cookie`
+		echo "WARNING: Save cookie file not found, Created Temp File [$SAVE_COOKIE]"
+	fi
+	
+	curl -s --cookie-jar "$COOKIE_DIR/$SAVE_COOKIE" -b "$COOKIE_DIR/$LOAD_COOKIE" --data "$QUERY_STRING" "$URL" > "$TEMP_FILE""_bak"
+	
+	# Remove the windows like carraige return. 
 	cat "$TEMP_FILE"_bak | tr -d "\015" > $TEMP_FILE 
 	rm -rf "$TEMP_FILE"_bak
-	FORM_FIELDS=`php -f regexFinder.php $TEMP_FILE '<input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="[^\"]*" />' | tr " " "\n" | grep "value" | tr "\"" "\n" | head -2 | tail -1` 
-	echo $FORM_FIELDS
-	echo ""
+}
 
-	page=2
+# Grabs the Form fields from a give crawled result. 
+# $PAGE the page you are currently retrieving.  
+# $PREVIOUS_TEMP_FILE the previous file which was crawled.  
+# @return QUERY_STRING overrides the Query String variable. 
+function grabFormFields {
+	PAGE_INDEX=$(($PAGE-2))
+	FORM_FIELDS=`php -f regexFinder.php $PREVIOUS_TEMP_FILE '<input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="[^\"]*" />' | tr " " "\n" | grep "value" | tr "\"" "\n" | head -2 | tail -1` 
 	FORM_FIELDS=$(python -c "import urllib; print urllib.urlencode({'__VIEWSTATE': '''$FORM_FIELDS'''})")
-	TEMP_FILE="$TEMP_DIR/temp_$page.html"
-	QUERY_STRING="__EVENTTARGET=&__EVENTARGUMENT=&__LASTFOCUS=&ctl00%24smMainScriptManager=&ctl00%24content%24ddlSort=price-low-to-high&ctl00%24content%24gvpDataPager%24ddlPageIndex=0&ctl00%24content%24gvpDataPager%24btnNext=%3E"'&'"$FORM_FIELDS"
-	curl -s --cookie-jar cookies2.txt -b cookies.txt --data "$QUERY_STRING" "http://www.franksalt.com.mt/Search/SearchResults.aspx" > "$TEMP_FILE"_bak
-	echo ""
-	echo ""
-	echo ""	
+	QUERY_STRING="__EVENTTARGET=&__EVENTARGUMENT=&__LASTFOCUS=&ctl00%24smMainScriptManager=&ctl00%24content%24ddlSort=price-low-to-high&ctl00%24content%24gvpDataPager%24ddlPageIndex=$PAGE_INDEX&ctl00%24content%24gvpDataPager%24btnNext=%3E"'&'"$FORM_FIELDS"
+	# Set the query string.  
+}
 
+PAGE="1"
+# Grab the first page.  
+grabPage "$PAGE"
+moreResults
 
-	cat "$TEMP_FILE"_bak | tr -d "\015" > $TEMP_FILE 
-	rm -rf "$TEMP_FILE"_bak
-	FORM_FIELDS=`php -f regexFinder.php $TEMP_FILE '<input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="[^\"]*" />' | tr " " "\n" | grep "value" | tr "\"" "\n" | head -2 | tail -1` 
-	echo $FORM_FIELDS
-
-	page=3
-	FORM_FIELDS=$(python -c "import urllib; print urllib.urlencode({'__VIEWSTATE': '''$FORM_FIELDS'''})")
-	TEMP_FILE="$TEMP_DIR/temp_$page.html"
-	QUERY_STRING="__EVENTTARGET=&__EVENTARGUMENT=&__LASTFOCUS=&ctl00%24smMainScriptManager=&ctl00%24content%24ddlSort=price-low-to-high&ctl00%24content%24gvpDataPager%24ddlPageIndex=1&ctl00%24content%24gvpDataPager%24btnNext=%3E"'&'"$FORM_FIELDS"
-
-
+while [ $NEXT -ne 0 ]
+do
+	PAGE=$(($PAGE + 1))
+	sleep "1s"
 	echo ""
-	echo ""
-	echo ""
-	curl -s --cookie-jar cookies3.txt -b cookies2.txt --data "$QUERY_STRING" "http://www.franksalt.com.mt/Search/SearchResults.aspx" > "$TEMP_FILE"_bak
-	cat "$TEMP_FILE"_bak | tr -d "\015" > $TEMP_FILE 
-	rm -rf "$TEMP_FILE"_bak
+	grabPage "$PAGE"
+	moreResults
+done
